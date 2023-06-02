7972c8733d6fc35a496bb6f4ca3709349b509554
中讯.梁晶涛 <liangjt05@dimpt.com>
2023-06-02 16:10:06 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import Format from "./src/format";
+import Validate from "./src/validate";
+
+export { Format, Validate };
---
+++ src/validate.js
@@ -0,0 +1,16 @@
+const formatToFixed = (money, decimals = 2) => {
+  return (
+    Math.round(
+      (parseFloat(money) + Number.EPSILON) * Math.pow(10, decimals)
+    ) / Math.pow(10, decimals)
+  ).toFixed(decimals);
+}
+const Format = {
+// 格式化金额展示： 12341234.246 -> $ 12,341,234.25
+formatMoney: (money, symbol = "", decimals = 2) =>
+  formatToFixed(money, decimals)
+    .replace(/\B(?=(\d{3})+\b)/g, ",")
+    .replace(/^/, `${symbol}`),
+};
+
+export default Format;
